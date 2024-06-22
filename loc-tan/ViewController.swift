5a73e823c60e37ee9878459b4c70ae217762bef2
Enchan <enchant0130@gmail.com>
2024-06-22 22:11:14 +0900

    [Update] ノッチの有無によってレイアウトを変化させる
    - キャンバスビューとトップバーの構成
    
    - ノッチのあるデバイスでは、キャンバスビューはトップバー下端まで広がる
    
    - ノッチのないデバイスでは、キャンパスビューはセーフエリア上端まで広がる
    
    - 切り替えは view.safeAreaInsets を参照して判断し、レイアウト制約の優先度を操作することで行なっている

--- loc-tan/ViewController.swift
+++ loc-tan/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /// ステータスバーを隠す
+    override var prefersStatusBarHidden: Bool {true}
+    
+    /// キャンバス上端からセーフエリアへの制約
+    @IBOutlet private weak var canvasTopConstraintToSafeArea: NSLayoutConstraint!
+    
+    /// キャンバス上端からトップバー下端への制約
+    @IBOutlet private weak var canvasTopConstraintToTopbar: NSLayoutConstraint!
+    
+    /// デバイスがノッチを持つかどうか
+    private var hasNotch: Bool {
+        return view.safeAreaInsets.bottom > 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    override func updateViewConstraints() {
+        // ノッチがあるならキャンバスをトップビューまで、なければセーフエリアまで広げる
+        canvasTopConstraintToSafeArea.priority = hasNotch ? .defaultLow : .defaultHigh
+        canvasTopConstraintToTopbar.priority = hasNotch ? .defaultHigh : .defaultLow
+        
+        super.updateViewConstraints()
+    }
 
 }
 
