60cd20c1f8c2bedc049d923ed7476a70638ccc19
Enchan <enchant0130@gmail.com>
2024-08-04 16:26:00 +0900

    [Update #13] セッションとプレビューの構成
    - Enchan1207/avkit_camera_capture から実装を一部移植

--- loc-tan/Camera/View/CameraView.swift
+++ loc-tan/Camera/View/CameraView.swift
@@ -6,15 +6,152 @@
 //
 
 import UIKit
+import AVKit
 
 class CameraView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    /// キャプチャプレビューレイヤ
+    let videoPreviewLayer = AVCaptureVideoPreviewLayer()
+    
+    /// 現在フォーカスが当たっている点の座標
+    var focusPoint: CGPoint? {
+        didSet {
+            if focusPoint != nil {
+                updateFocusFrame(to: focusPoint!)
+            }else{
+                removeFocusFrame()
+            }
+        }
     }
-    */
-
+    
+    /// プレビュー対象のキャプチャセッション
+    var session: AVCaptureSession? {
+        get { videoPreviewLayer.session }
+        set { videoPreviewLayer.session = newValue }
+    }
+    
+    // MARK: - Private
+    
+    // グリッド
+    private var grid = CAShapeLayer()
+    
+    /// フォーカス枠
+    private var focusFrame = CAShapeLayer()
+    
+    // MARK: - Initializing
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+    
+    private func setup(){
+        // カメラプレビューレイヤを生成・割り当て
+        videoPreviewLayer.videoGravity = .resizeAspectFill
+        layer.addSublayer(videoPreviewLayer)
+        
+        // グリッド枠を初期化
+        initGrid()
+        layer.addSublayer(grid)
+        
+        // フォーカス枠を初期化
+        initFocusFrame()
+        layer.addSublayer(focusFrame)
+        
+        setNeedsLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        videoPreviewLayer.frame = bounds
+        grid.path = createGridLayer(size: bounds.size, numberOfRows: 3, numberOfCols: 3)
+    }
+    
+    override func didMoveToSuperview() {
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    // MARK: - Methods
+    
+    /// フォーカス枠を初期化する
+    private func initFocusFrame(){
+        focusFrame = .init()
+        focusFrame.strokeColor = UIColor.systemOrange.cgColor
+        focusFrame.lineWidth = 1.5
+        focusFrame.fillColor = UIColor.clear.cgColor
+        focusFrame.opacity = 0.0
+    }
+    
+    /// グリッドを初期化する
+    private func initGrid(){
+        grid = .init()
+        grid.strokeColor = UIColor.white.cgColor
+        grid.lineWidth = 0.9
+        grid.fillColor = UIColor.clear.cgColor
+        grid.opacity = 0.9
+    }
+    
+    /// グリッドレイヤを作成して返す
+    /// - Parameters:
+    ///   - size: 領域の大きさ
+    ///   - rows: 縦区画数
+    ///   - cols: 横区画数
+    /// - Returns: 作成したグリッドレイヤ
+    /// - Note: 区画数の数で分割されます(引かれる線分は指定した値-1本になります)。
+    private func createGridLayer(size: CGSize, numberOfRows rows: UInt, numberOfCols cols: UInt) -> CGPath {
+        let path = CGMutablePath()
+        guard rows > 1, cols > 1 else {return path}
+        
+        // 1区画あたりの幅と高さを計算
+        let unitWidth = size.width / .init(cols)
+        let unitHeight = size.height / .init(rows)
+        
+        // 引く線分の座標リストを計算
+        let lineXPositions = (1..<cols).map({unitWidth * .init($0)})
+        let lineYPositions = (1..<rows).map({unitHeight * .init($0)})
+        
+        // それぞれについて move -> addLine
+        lineXPositions.forEach({x in
+            path.move(to: .init(x: x, y: 0))
+            path.addLine(to: .init(x: x, y: size.height))
+        })
+        lineYPositions.forEach({y in
+            path.move(to: .init(x: 0, y: y))
+            path.addLine(to: .init(x: size.width, y: y))
+        })
+        
+        return path
+    }
+    
+    /// フォーカス枠の位置を更新する
+    /// - Parameter layerPoint: レイヤ上のフォーカス位置
+    private func updateFocusFrame(to layerPoint: CGPoint){
+        // TODO: この辺りは完全にデザインの問題なので、いつかちゃんとやる
+        let size: CGFloat = 100
+        let rect = CGRect(x: layerPoint.x - size / 2, y: layerPoint.y - size / 2, width: size, height: size)
+        focusFrame.path = UIBezierPath(rect: rect).cgPath
+        
+        let animation = CABasicAnimation(keyPath: "opacity")
+        animation.fromValue = 0.0
+        animation.toValue = 1.0
+        animation.duration = 0.3
+        animation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        
+        // トランジション終了後に値が戻るのを防ぐ
+        animation.isRemovedOnCompletion = false
+        animation.fillMode = .forwards
+        
+        focusFrame.add(animation, forKey: "opacity")
+    }
+    
+    /// フォーカス枠を削除する
+    private func removeFocusFrame(){
+        focusFrame.opacity = 0.0
+    }
+    
 }
--- loc-tan/Camera/ViewController/CameraViewController.swift
+++ loc-tan/Camera/ViewController/CameraViewController.swift
@@ -6,24 +6,143 @@
 //
 
 import UIKit
+import AVKit
 
 class CameraViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    /// 現在のズーム倍率
+    private(set) var currentZoomFactor: CGFloat = 1.0
+    
+    // MARK: - Private
+    
+    /// キャプチャセッション
+    private let session = AVCaptureSession()
+    
+    /// 写真出力
+    private let photoOutput = AVCapturePhotoOutput()
+    
+    /// 現在の入力デバイス
+    private var currentInputDevice: AVCaptureDevice? { (session.inputs.first as? AVCaptureDeviceInput)?.device }
+    
+    /// ズーム開始時の倍率
+    private var initialZoomFactor: CGFloat = 1.0
+    
+    private var cameraView: CameraView { view as! CameraView }
+    
+    // MARK: - View lifecycles
+    
+    override func loadView() {
+        self.view = CameraView(frame: .zero)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // ジェスチャを構成
+        [UITapGestureRecognizer(target: self, action: #selector(onTap)),
+         UIPinchGestureRecognizer(target: self, action: #selector(onPinch))].forEach(cameraView.addGestureRecognizer)
+        
+        // セッションを構成
+        configureCaptureSession(session)
+        cameraView.session = session
     }
-    */
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // セッション開始
+        DispatchQueue.global().async{[weak self] in
+            self?.session.startRunning()
+        }
+    }
+    
+    /// キャプチャセッションを構成
+    private func configureCaptureSession(_ session: AVCaptureSession){
+        session.beginConfiguration()
+        
+        // セッションプリセットを設定
+        session.sessionPreset = .hd4K3840x2160
+        
+        // デフォルトのキャプチャデバイスを取得
+        guard let device = AVCaptureDevice.default(for: .video),
+              let deviceInput = try? AVCaptureDeviceInput(device: device) else {
+            session.commitConfiguration()
+            return
+        }
+        
+        // 入出力の構成
+        if(session.canAddInput(deviceInput)){
+            session.addInput(deviceInput)
+        }
+        if session.canAddOutput(photoOutput){
+            session.addOutput(photoOutput)
+        }
+        
+        session.commitConfiguration()
+    }
+    
+    // MARK: - Gestures
+    
+    @objc private func onTap(_ gesture: UITapGestureRecognizer){
+        // タップ位置をレイヤ座標系に変換し、デバイスのフォーカス位置を移動
+        let viewTapPoint = gesture.location(in: cameraView)
+        let layerTapPoint = cameraView.videoPreviewLayer.captureDevicePointConverted(fromLayerPoint: viewTapPoint)
+        updateDeviceFocusPoint(to: layerTapPoint)
+        
+        // プレビューViewのフォーカス位置を再設定
+        cameraView.focusPoint = viewTapPoint
+    }
+    
+    @objc private func onPinch(_ gesture: UIPinchGestureRecognizer){
+        guard let device = currentInputDevice else {return}
+        
+        // ズーム開始時のスケールを保持しておく
+        if gesture.state == .began {
+            initialZoomFactor = device.videoZoomFactor
+        }
+        
+        // ジェスチャのスケールからデバイスに渡すスケールを計算し、受理可能な値にクリップする
+        // なお最大スケールに関してはやたらデカい値が許容されるので、アプリ側で最大値を設けておく
+        let minScale = device.minAvailableVideoZoomFactor
+        let maxScale = min(device.maxAvailableVideoZoomFactor, 20.0)
+        let newScale = min(max(initialZoomFactor * gesture.scale, minScale), maxScale)
+        currentZoomFactor = newScale
+        
+        // デバイスに設定を反映
+        updateDeviceZoomFactor(to: newScale)
+        
+        // TODO: Notify to parent
+    }
+    
+    // MARK: - Methods
+    
+    // TODO: async?
+    private func updateDeviceFocusPoint(to point: CGPoint){
+        guard let device = currentInputDevice else {return}
+        do {
+            try device.lockForConfiguration()
+            if device.isFocusPointOfInterestSupported {
+                device.focusPointOfInterest = point
+                device.focusMode = .autoFocus
+            }
+            if device.isExposurePointOfInterestSupported {
+                device.exposurePointOfInterest = point
+                device.exposureMode = .autoExpose
+            }
+            device.unlockForConfiguration()
+        } catch {
+            print("Failed to focus: \(error)")
+        }
+    }
+    
+    // TODO: async?
+    private func updateDeviceZoomFactor(to scale: CGFloat){
+        guard let device = currentInputDevice else {return}
+        do {
+            try device.lockForConfiguration()
+            device.videoZoomFactor = scale
+            device.unlockForConfiguration()
+        } catch {
+            print("Failed to zoom: \(error)")
+        }
+    }
+    
 }
