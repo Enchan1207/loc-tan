d1bdce2cc2c87a7873845a681b9dd1f49d3e661d
Enchan <enchant0130@gmail.com>
2024-08-04 13:44:41 +0900

    [Add #13] Camera view
    - カメラプレビュー用のViewとVCを追加

---
+++ loc-tan/Camera/View/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  loc-tan
+//
+//  Created by EnchantCode on 2024/08/04.
+//
+
+import UIKit
+
+class CameraView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
